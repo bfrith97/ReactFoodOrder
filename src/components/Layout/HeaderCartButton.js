317079ae440cb7e9012960a5dd345b66d6e870fb
Brandon <brandonfrith@hotmail.co.uk>
2023-02-15 11:42:37 +0000

    finish project

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,17 +1,42 @@
-
+import {useContext, useEffect, useState} from "react";
 
 import CartIcon from "../Cart/CartIcon";
+import CartContext from "../../store/cart-context";
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = props => {
-    return <button className={classes.button} onClick={props.onClick}>
+    const [btnIsHiglighted, setBtnIsHighligted] = useState(false)
+    const cartCtx = useContext(CartContext);
+    const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+        return curNumber + item.amount;
+    }, 0);
+
+    const {items} = cartCtx
+
+    const btnClasses = `${classes.button} ${btnIsHiglighted ? classes.bump : ''}`;
+
+    useEffect(() => {
+        if (items.length === 0) {
+            return;
+        }
+        setBtnIsHighligted(true)
+
+        const timer = setTimeout(() => {
+            setBtnIsHighligted(false)
+        }, 300);
+
+        return () => {
+            clearTimeout(timer);
+        }
+
+    }, [items])
+
+    return <button className={btnClasses} onClick={props.onClick}>
         <span className={classes.icon}>
             <CartIcon/>
         </span>
         <span>Your Cart</span>
-        <span className={classes.badge}>
-            (3 Items)
-        </span>
+        <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
 };
 
